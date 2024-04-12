fb55c81ae87bf3d5b32a6c9f4d771f3f35e86fd9
kipkemoi Haron <kipkemoiharon4@gmail.com>
2024-04-12 10:54:23 +0300

    completed assignment

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Event listener for the search button click
+    const searchBtn = document.getElementById('search-btn');
+    const searchBtn = document.getElementById('search-btn');
+
+    // Event listener for the recipe close button click
